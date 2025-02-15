d626b517956fa19b5e88d8f8cc2c62587614e659
Kavya Mittal <mister1729x@gmail.com>
2025-02-15 11:12:55 +0530

    making context api for easy accessing chatpage all over application using react

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -54,6 +54,25 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
+   ///api/user
+const allUsers = asyncHandler(async(req,res)=>{
+  const keyword = req.query.search ?{
+    $or:[
+      {name:{
+        $regex:req.query.search,
+        $options:'i',
+      }},
+      {email:{
+        $regex:req.query.search,
+        $options:'i',
+      }},
+    ],
+  }
+  :{};
+  // console.log(keyword);
+  const users= await User.find(keyword).find({_id:{$ne:req.user._id}});
+  res.send(users);  
+});
 
-module.exports = { registerUser, authUser };
+module.exports = { registerUser, authUser,allUsers };
  
--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+// import { setUser } from "../../utils/localStorage";
 import {
   VStack,
   FormControl,
@@ -13,8 +17,61 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useState(false);
+  const[loading, setLoading] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
   const handleClick = () => setShow(!show);
-  const submitHandler = () => {};
+  
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
+
 
   return (
     <VStack spacing="5px" color="white">
@@ -26,6 +83,7 @@ const Login = () => {
           placeholder="Enter Your Email"
           onChange={(e) => setEmail(e.target.value)}
           color="white"
+          value={email}
           placeholderColor="white"
           _placeholder={{ color: "white", fontWeight: "bold", fontSize: "lg" }}
         />
@@ -40,6 +98,7 @@ const Login = () => {
             placeholder="Enter Your Password"
             onChange={(e) => setPassword(e.target.value)}
             color="white"
+            value={password}
             placeholderColor="white"
             _placeholder={{
               color: "white",
@@ -59,6 +118,7 @@ const Login = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Login
       </Button>
